243b00955cc7b95c55069f44e0e52e7d4dd72a1c
ekingery <ekingery@gmail.com>
2009-10-01 00:12:52 +0800

    Added functionality to traverse subdirectories in a unit test folder

--- tests.php
+++ tests.php
@@ -15,16 +15,30 @@ Class Tests
 
 		$suite = new PHPUnit_Framework_TestSuite();
 
-		foreach($files as $file)
-		{
-			if(is_file($file) AND ! strpos($file, 'Base_Test'.EXT))
-			{
-				PHPUnit_Util_Filter::addFileToFilter($file);
-				$suite->addTestFile($file);
-			}
-		}
+		PHPUnit_Util_Filter::addDirectoryToFilter(MODPATH);
+		PHPUnit_Util_Filter::addDirectoryToFilter(SYSPATH);
+		self::addTests($suite, $files);
 
 		return $suite;
+	}
+	
+	static function addTests($suite, $files)
+	{
+		foreach($files as $file)
+		{
+			if(is_array($file))
+			{
+				self::addTests($suite, $file);
+			} 
+			else 
+			{		
+				if(is_file($file) AND ! strpos($file, 'Base_Test'.EXT))
+				{
+					PHPUnit_Util_Filter::addFileToFilter($file);
+					$suite->addTestFile($file);
+				}
+			}
+		}
 	}
 
 }
