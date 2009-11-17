69953cefd195c3294aff36a7daeaa729089f8025
Matt <matt@dev-desktop.(none)>
2009-11-17 20:13:43 +0000

    Adding CLI tests

---
+++ phpunit_tests/kohana/CLITest.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Unit tests for CLI
+ *
+ * Based on the Kohana-unittest test
+ *
+ * @group kohana
+ * @group kohana.cli
+ * @see CLI
+ * @author BRMatt <matthew@sigswitch.com>
+ * @author Shadowhand 
+ */
+Class Kohana_CLI_Test extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var array $_SERVER before each test
+	 */
+	protected $_server = array();
+
+	/**
+	 * @var array An array of arguments to put in $_SERVER['argv']
+	 */
+	protected $options = array(
+							'--uri' => 'test/something',
+							'--we_are_cool',
+							'invalid option',
+							'--version' => '2.23'
+						);
+
+	/**
+	 * Setup the enviroment for each test
+	 */
+	function setUp()
+	{
+		$this->_server = $_SERVER;
+		
+		$_SERVER['argv'] = array('index.php');
+
+		foreach($this->options as $option => $value)
+		{
+			if(is_string($option))
+			{
+				$_SERVER['argv'][] = $option.'='.$value;
+			}
+			else
+			{
+				$_SERVER['argv'][] = $value;
+			}
+		}
+
+		$_SERVER['argc'] = count($_SERVER['argv']);
+	}
+
+	/**
+	 * Return the enviroment to its original state
+	 */
+	function tearDown()
+	{
+		$_SERVER = $this->_server;
+
+		$this->_server = array();
+	}
+
+
+	/**
+	 * Tests CLI::options()
+	 *
+	 * Options should only parse arguments requested
+	 *
+	 * @test
+	 */
+	function testOptionsOnlyParsesWantedArguments()
+	{
+		$options = CLI::options('uri');
+
+		$this->assertSame(1, count($options));
+		
+		$this->assertArrayHasKey('uri', $options);
+		$this->assertSame($options['uri'], $this->options['--uri']);
+	}
+
+	/**
+	 * Tests CLI::options()
+	 *
+	 * Options should not parse invalid arguments (i.e. not starting with --_
+	 *
+	 * @test
+	 */
+	function testOptionsDoesNotParseInvalidArguments()
+	{
+		$options = CLI::options('uri', 'invalid');
+		
+		$this->assertSame(1, count($options));
+		$this->assertArrayHasKey('uri', $options);
+		$this->assertArrayNotHasKey('invalid', $options);
+	}
+
+	/**
+	 * Tests CLI::options()
+	 *
+	 * Options should parse multiple arguments & values correctly
+	 *
+	 * @test
+	 */
+	function testOptionsParsesMultipleArguments()
+	{
+		$options = CLI::options('uri', 'version');
+
+		$this->assertSame(2, count($options));
+		$this->assertArrayHasKey('uri', $options);
+		$this->assertArrayHasKey('version', $options);
+		$this->assertSame($this->options['--uri'], $options['uri']);
+		$this->assertSame($this->options['--version'], $options['version']);
+	}
+
+	/**
+	 * Tests CLI::options()
+	 *
+	 * Options should parse arguments without specified values as NULL
+	 *
+	 * @test
+	 */
+	function testOptionsParsesArgumentsWithoutValueAsNull()
+	{
+		$options = CLI::options('uri', 'we_are_cool');
+
+		$this->assertSame(2, count($options));
+		$this->assertSame(NULL, $options['we_are_cool']);
+	}
+}
