69cbba3dc34bbfc76d977a06e2e8f005b4f82b05
BMatt <matthew@sigswitch.com>
2009-12-01 03:54:24 -0700

    Changed config as per banks' suggestions, see http://forum.kohanaphp.com/comments.php?DiscussionID=4047&page=2#Item_4

--- config/phpunit.php
+++ config/phpunit.php
@@ -1,18 +1,16 @@
 <?php
 
-
-// We use 'use_' switches to make it easier to turn on/off certain settings
 return array
 (
 	// This is the folder where we generate and zip all the reports for downloading
 	// Needs to be readable and writable
-	'temp_path'				=> APPPATH.'cache/phpunit/',
+	'temp_path'				=> Kohana::$cache_dir.'/phpunit',
 
 	// Should the code coverage checkbox be selected by default for the web ui
 	'coverage_selected'		=> TRUE,
 
-	// If you don't use a whitelist then by default everything that isn't blacklisted is included
-	// If you do, then only whitelisted items are included
+	// If you don't use a whitelist then only files included during the request will be counted
+	// If you do, then only whitelisted items will be counted
 	'use_whitelist'		=> TRUE,
 
 	'whitelist'	=>	array
@@ -23,8 +21,8 @@ return array
 						// Set to TRUE to include all modules, or use an array of module names
 						// (the keys of the array passed to Kohana::modules() in the bootstrap)
 						'modules'	=> TRUE,
-						// If you don't want the kohana code coverage reports to pollute your app's
-						// Then set this to true
+						// If you don't want the kohana code coverage reports to pollute your app's,
+						// then set this to FALSE
 						'system'	=> TRUE,
 					),
 	
