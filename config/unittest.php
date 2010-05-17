c945b011ad7407c15d58eedab22de0f1ab1c3ee1
Michal Kocian <michal@kocian.name>
2010-05-18 05:28:36 +0800

    Config file clean up

--- config/unittest.php
+++ config/unittest.php
@@ -1,55 +1,54 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
-return array
-(
+return array(
+
 	// The only enviroment in which the web runner is allowed to run
 	// You can run tests from phpunit cli command reguardless of this setting
 	// This can also be set to an array for multiple enviroments
-	'enviroment'		=> Kohana::DEVELOPMENT,
+	'enviroment' => Kohana::DEVELOPMENT,
 
 	// This is the folder where we generate and zip all the reports for downloading
 	// Needs to be readable and writable
-	'temp_path'				=> Kohana::$cache_dir.'/unittest',
+	'temp_path' => Kohana::$cache_dir.'/unittest',
 
-	// Path from DOCROOT (i.e. http://yoursite/) to the folder where HTML cc reports can be 
-	// published.  If you'd prefer not to allow users to do this then simply set the value
-	// to FALSE.
+	// Path from DOCROOT (i.e. http://yourdomain/) to the folder where HTML cc reports can be published.
+        // If you'd prefer not to allow users to do this then simply set the value to FALSE.
 	//
-	// Example value of 'cc_report' would allow devs to see report at http://yourdomain/cc_report/
+	// Example value of 'cc_report_path' would allow devs to see report at http://yourdomain/report/
 	'cc_report_path' => 'report',
 
 	// If you don't use a whitelist then only files included during the request will be counted
 	// If you do, then only whitelisted items will be counted
-	'use_whitelist'		=> TRUE,
+	'use_whitelist' => TRUE,
 
 	// Items to whitelist, only used in cli
 	// Web runner ui allows user to choose which items to whitelist
-	'whitelist'	=>	array
-					(
-						// Should the app be whitelisted?
-						// Useful if you just want to test your application
-						'app'		=> TRUE,
-						// Set to array(TRUE) to include all modules, or use an array of module names
-						// (the keys of the array passed to Kohana::modules() in the bootstrap)
-						// Or set to FALSE to exclude all modules
-						'modules'	=> array(TRUE),
-						// If you don't want the kohana code coverage reports to pollute your app's,
-						// then set this to FALSE
-						'system'	=> TRUE,
-					),
+	'whitelist' => array(
+
+		// Should the app be whitelisted?
+		// Useful if you just want to test your application
+		'app' => TRUE,
+
+		// Set to array(TRUE) to include all modules, or use an array of module names
+		// (the keys of the array passed to Kohana::modules() in the bootstrap)
+		// Or set to FALSE to exclude all modules
+		'modules' => array(TRUE),
+
+		// If you don't want the kohana code coverage reports to pollute your app's,
+		// then set this to FALSE
+		'system' => TRUE,
+	),
 	
 	// Does what it says on the tin
 	// Blacklisted files won't be included in code coverage reports
 	// If you use a whitelist then the blacklist will be ignored
-	'use_blacklist'		=> FALSE,
+	'use_blacklist' => FALSE,
 
-	'blacklist'	=>	array
-					(
-						// List of individual files / folders to blacklist
-					),
+	// List of individual files / folders to blacklist
+	'blacklist' => array(
+	),
 	
 	// A database connection that can be used when testing
-	// This doen'st overwrite anything, tests will have to use this value manually
-	'db_connection'	=> 'unit_testing',
-
+	// This doesn't overwrite anything, tests will have to use this value manually
+	'db_connection' => 'unittest',
 );
