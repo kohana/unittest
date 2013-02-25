a949f09f3100f7d10bd4a86e8abc7b529ff01302
Lorenzo Pisani <zeelot3k@gmail.com>
2013-02-24 16:31:07 -0800

    run type through strtolower in if statement as well (refs #4507)

--- classes/Kohana/Unittest/Database/TestCase.php
+++ classes/Kohana/Unittest/Database/TestCase.php
@@ -89,7 +89,7 @@ abstract class Kohana_Unittest_Database_TestCase extends PHPUnit_Extensions_Data
 		// Get the unittesting db connection
 		$config = Kohana::$config->load('database.'.$this->_database_connection);
 
-		if($config['type'] !== 'pdo')
+		if(strtolower($config['type']) !== 'pdo')
 		{
 			$config['connection']['dsn'] = strtolower($config['type']).':'.
 			'host='.$config['connection']['hostname'].';'.
